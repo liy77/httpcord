a3f492ffa5620f31223e4e788ca1e6b0b9b93921
JustAWaifuHunter <hcsbrian4056@gmail.com>
2022-07-15 14:50:07 -0300

    refactor(emoji): Use snowflake instead role

--- emoji.go
+++ emoji.go
@@ -1,14 +1,14 @@
 package httpcord
 
 type Emoji struct {
-	ID            Snowflake `json:"id,omitempty"`
-	Name          string    `json:"name,omitempty"`
-	Roles         []*Role   `json:"roles,omitempty"`
-	User          *User     `json:"user,omitempty"`
-	RequireColons bool      `json:"require_colons,omitempty"`
-	Managed       bool      `json:"managed,omitempty"`
-	Animated      bool      `json:"animated,omitempty"`
-	Available     bool      `json:"available,omitempty"`
+	ID            Snowflake    `json:"id,omitempty"`
+	Name          string       `json:"name,omitempty"`
+	Roles         []*Snowflake `json:"roles,omitempty"`
+	User          *User        `json:"user,omitempty"`
+	RequireColons bool         `json:"require_colons,omitempty"`
+	Managed       bool         `json:"managed,omitempty"`
+	Animated      bool         `json:"animated,omitempty"`
+	Available     bool         `json:"available,omitempty"`
 }
 
 func (e *Emoji) Mention() string {
